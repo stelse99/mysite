a061691fa58902ada1557ed125c1306c143fb428
stelse99 <stelse99@gmail.com>
2023-01-02 21:42:34 +0900

    Login 구현

--- pybo/urls.py
+++ pybo/urls.py
@@ -5,7 +5,7 @@ app_name = 'pybo'
 
 urlpatterns = [
     # path('', views.product),
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  # 'pybo/' 에 해당되는 path
     path('<int:question_id>/', views.detail, name='detail'),
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
     path('question/create/', views.question_create, name='question_create'),
